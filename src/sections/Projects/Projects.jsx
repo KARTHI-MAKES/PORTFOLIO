6045880205f1087784535a8e9e5c7d698f42d776
KARTHI-MAKES <karthiyamunarani5.com>
2024-09-26 14:36:28 +0530

    added 1 project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import styles from './Projects.module.css';
 import NEWS from '../../assets/NEWS.jpeg';
 import CHAT from '../../assets/chat logo.jpeg'
 import ProjectCard from '../../common/ProjectCard';
+import PORT from'../../assets/portfolio.jpeg'
 
 function Projects() {
   return (
@@ -19,6 +20,12 @@ function Projects() {
       link="https://github.com/KARTHI-MAKES/REACT-CHATAPP"
       h3="CHAT APP"
       p ="CHAT WEBSITE"
+    />
+    <ProjectCard 
+      src={PORT}
+      link="https://github.com/KARTHI-MAKES/PORTFOLIO"
+      h3="PORTFOLIO"
+      p ="MY PORTFOLIO"
     />
    </div>
    <hr />
